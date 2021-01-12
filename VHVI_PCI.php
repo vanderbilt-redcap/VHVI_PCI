e17ceec03271383e09665327c377268454a79cc8
Carl Reed <carlreedw@gmail.com>
2021-01-12 15:54:49 -0600

    adds local log function and PHPSpreadsheet cathpci_import creates workbook object

--- VHVI_PCI.php
+++ VHVI_PCI.php
@@ -1,5 +1,18 @@
 <?php
 namespace Vanderbilt\VHVI_PCI;
 class VHVI_PCI extends \ExternalModules\AbstractExternalModule {
-	
+	function llog($text) {
+		// only works on my local
+		if (!file_exists($this->getModulePath() . 'able_test.php')) {
+			return;
+		}
+		
+		// create new file first time this is run, then append for additional calls
+		if (isset($this->ran_llog)) {
+			file_put_contents("C:/vumc/log.txt", "$text\n", FILE_APPEND);
+		} else {
+			$this->ran_llog = true;
+			file_put_contents("C:/vumc/log.txt", "VHVI_PCI llog created: " . date('c') . "\n$text\n");
+		}
+	}
 }
--- cathpci_import.php
+++ cathpci_import.php
@@ -1,23 +1,14 @@
 <?php
 require_once APP_PATH_DOCROOT . 'ProjectGeneral/header.php';
-echo "<br>";
-echo "\$_GET: ";
-echo "<br>";
-print_r($_GET);
-echo "<br>";
-echo "<br>";
-echo "\$_POST: ";
-echo "<br>";
-print_r($_POST);
-echo "<br>";
-echo "<br>";
-echo "\$_FILES: ";
-echo "<br>";
-print_r($_FILES);
-echo "<br>";
 
-$target_url = "http://localhost/redcap/redcap_v10.6.4/ExternalModules/?prefix=VHVI_PCI_Registry&page=cathpci_upload&pid=73";
+$module->llog("catpci_import.php");
+$module->llog("\$_GET:\n" . print_r($_GET, true));
+$module->llog("\$_POST:\n" . print_r($_POST, true));
+$module->llog("\$_FILES:\n" . print_r($_FILES, true));
+
 ?>
+
+<?php if (!isset($_POST['submit'])) { ?>
 <div id="container">
 	<form method="post" enctype="multipart/form-data">
 		<span>Select a CathPCI workbook to upload:<span>
@@ -27,6 +18,24 @@ $target_url = "http://localhost/redcap/redcap_v10.6.4/ExternalModules/?prefix=VH
 		<input type="submit" value="Upload" name="submit">
 	</form>
 </div>
+<?php } else { 
+	require 'vendor/autoload.php';
+	try {
+		$reader = \PhpOffice\PhpSpreadsheet\IOFactory::createReader("Xlsx");
+		$workbook = $reader->load($_FILES["cathpci_doc"]["tmp_name"]);
+	} catch(\PhpOffice\PhpSpreadsheet\Reader\Exception $e) {
+		// \REDCap::logEvent("DPP import failure", "PhpSpreadsheet library errors -> " . print_r($e, true) . "\n", null, $rid, $eid, PROJECT_ID);
+		exit("There was an issue loading the CathPCI workbook: $e");
+	}
+	$workbook_loaded = true;
+?>
+<span>Imported workbook</span>
+<br>
+<?php 
+	if ($workbook_loaded) {
+		echo "<span>Workbook loaded successfully</span>";
+	}
+} ?>
 <?php
 require_once APP_PATH_DOCROOT . 'ProjectGeneral/footer.php';
 ?>
